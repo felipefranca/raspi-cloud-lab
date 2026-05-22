092e3368b561f1ba6c6e008d3c2832ee01fcda72
Felipe França <felipejontaoliveira@gmail.com>
2026-05-22 14:28:45 -0300

    fet - Including CD configuration and new controller to say hello

--- src/main/java/com/docker/cloud_learn/MessageController.java
+++ src/main/java/com/docker/cloud_learn/MessageController.java
@@ -5,8 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/messages")
@@ -21,8 +22,14 @@ public class MessageController {
     }
 
     @PostMapping
-    public ResponseEntity<Message> save(@RequestBody Message message){
+    public ResponseEntity<Message> save(@RequestBody Message message) {
         var result = repository.save(message);
         return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        return "Hello! " + LocalDateTime.now().format(formatter);
     }
 }
