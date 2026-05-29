f04a0d4e2b90981c84bb698aa826855ace06da20
Felipe França <felipejontaoliveira@gmail.com>
2026-05-29 10:24:11 -0300

    feat: add correlation id filter

---
+++ src/main/java/com/docker/raspi_cloud_lab/config/CorrelationIdFilter.java
@@ -0,0 +1,46 @@
+package com.docker.raspi_cloud_lab.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class CorrelationIdFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String correlationId = UUID.randomUUID().toString();
+
+        MDC.put("correlationId", correlationId);
+
+        try {
+
+            response.setHeader(
+                    "X-Correlation-ID",
+                    correlationId
+            );
+
+            filterChain.doFilter(
+                    request,
+                    response
+            );
+
+        } finally {
+
+            MDC.clear();
+
+        }
+    }
+}
