d8d7988e0cfc561539fbd8a0d20bc419648b61c2
Felipe França <felipejontaoliveira@gmail.com>
2026-05-29 09:57:58 -0300

    feat: add logstash to controller

--- src/main/java/com/docker/raspi_cloud_lab/MessageController.java
+++ src/main/java/com/docker/raspi_cloud_lab/MessageController.java
@@ -1,5 +1,7 @@
 package com.docker.raspi_cloud_lab;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,17 +15,20 @@ import java.util.List;
 @RequestMapping("api/v1/messages")
 public class MessageController {
 
+    private static final Logger log = LoggerFactory.getLogger(MessageController.class);
     @Autowired
     private MessageRepository repository;
 
     @GetMapping
     public List<Message> hello() {
+        log.info("Todas as mensagens recuperadas");
         return repository.findAll();
     }
 
     @PostMapping
     public ResponseEntity<Message> save(@RequestBody Message message) {
         var result = repository.save(message);
+        log.info("Mensagem criada");
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
